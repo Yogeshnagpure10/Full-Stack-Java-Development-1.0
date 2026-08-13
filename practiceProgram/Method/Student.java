33f0d91bd3f26d8ca0a71a28ca95af4a2535ed12
Yogeshwar Nagpure <ynagpure1994@gmail.com>
2026-08-13 23:58:51 +0530

    comment added

---
+++ practiceProgram/Method/Student.java
@@ -0,0 +1,17 @@
+package Method;
+
+public class Student {
+
+    public static void sum()
+    {
+        int a = 10 ;
+        int b = 20 ;
+        System.out.println(a+b);
+    }
+
+    public static void main(String [] args)
+    {
+        sum();
+    }
+    
+}
