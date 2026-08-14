971ace21f860dcff26f4fd93b7bf553db6455722
Yogeshwar Nagpure <ynagpure1994@gmail.com>
2026-08-14 19:35:08 +0530

    comment added

---
+++ practiceProgram/Method/Student2.java
@@ -0,0 +1,19 @@
+package Method;
+
+public class Student2 {
+
+    public static double mull()
+    {
+        int a = 10;
+        int b = 20;
+
+        return a*b;
+    }
+
+
+    public static void main(String [] args)
+    {
+        System.out.println(mull());
+    }
+    
+}
