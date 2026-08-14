9a303880c5c498138c8ffd51bb368aedf8fe5d05
Yogeshwar Nagpure <ynagpure1994@gmail.com>
2026-08-14 19:42:07 +0530

    comment added

---
+++ practiceProgram/Method/StudentName.java
@@ -0,0 +1,17 @@
+package Method;
+
+public class StudentName {
+
+    public static String name()
+    {
+       String a = "Yogesh";
+
+        return a;
+    }
+
+    public static void main(String [] args)
+    {
+        System.out.println(name());
+    }
+    
+}
