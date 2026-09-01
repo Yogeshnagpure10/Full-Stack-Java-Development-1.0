45d2cb8b4d5122f2002b8f93f734e55cc1f4439b
Yogeshwar Nagpure <ynagpure1994@gmail.com>
2026-09-01 21:44:54 +0530

    comment added

---
+++ practiceProgram/Method/MethodOverloading2.java
@@ -0,0 +1,31 @@
+
+import java.util.Scanner;
+
+public class MethodOverloading2 {
+
+    int add(int a, int b)
+    {
+        return a+b;
+    }
+
+  
+    
+
+    public static void main(String [] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the Number");
+
+        int num = sc.nextInt();
+
+        
+
+        
+
+
+
+
+
+    }
+}
