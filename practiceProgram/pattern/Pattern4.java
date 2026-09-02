db4a01ca1c7b931632d27ea5e6c1fdd28e221d54
Yogeshwar Nagpure <ynagpure1994@gmail.com>
2026-09-02 23:54:55 +0530

    comment pattern4 added

---
+++ practiceProgram/pattern/Pattern4.java
@@ -0,0 +1,38 @@
+/**
+ 
+       *
+     * *
+   * * *
+ * * * *
+ */
+
+
+import java.util.Scanner;
+public class Pattern4 {
+
+    public static void main(String [] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Right side bottom Star pattern");
+        System.out.println("Enter n:");
+
+        int n = sc.nextInt();
+
+        for(int i=1; i<=n; i++)
+        {
+            for(int j=1; j<=n; j++)
+            {
+                if(i+j>n)
+                {
+                    System.out.print("* ");
+                }
+                else
+                {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    
+}
