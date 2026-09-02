7adc930dabdc0c352f2365ad41110268a5bd392c
Yogeshwar Nagpure <ynagpure1994@gmail.com>
2026-09-02 23:57:37 +0530

    comment pattern7 added

---
+++ practiceProgram/pattern/Pattern7.java
@@ -0,0 +1,39 @@
+/**
+ *                  * 
+    *            *
+       *     *
+          *
+ * Pattern7
+ */
+
+import java.util.Scanner;
+public class Pattern7 {
+
+    public static void main(String [] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter n:");
+        int n = sc.nextInt();
+
+        for(int i=0 ; i< n ; i++ )
+        {
+            for(int j=0 ; j<i ; j++)
+            {
+                System.out.print("  ");
+            }
+                System.out.print("*");
+                for(int j =0; j<2 *(n-i-1);j++)
+                {
+                    System.out.print("  ");
+                }
+                if(i != n-1)
+                {
+                    System.out.print("* ");
+                }
+                 System.out.println();
+            }
+           
+        }
+    }
+    
+
