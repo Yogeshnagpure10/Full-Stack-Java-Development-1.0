ff0e0872ae0a12d7683c5a7f8bc7f042db31595d
Yogeshwar Nagpure <ynagpure1994@gmail.com>
2026-09-02 23:53:36 +0530

    comment pattern1 added

---
+++ practiceProgram/pattern/Pattern1.java
@@ -0,0 +1,36 @@
+/**
+ 
+ * 
+ **
+ ***
+ **** 
+
+ */
+
+import java.util.Scanner;
+
+public class Pattern1 {
+
+
+    public static void main(String [] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Show the Star Pattern 1 to 4 ");
+
+        System.out.println("Enter n :");
+
+        int n = sc.nextInt();
+
+        for(int i=1; i<=n; i++)
+        {
+            for(int j=1; j<=i; j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+    }
+    
+}
