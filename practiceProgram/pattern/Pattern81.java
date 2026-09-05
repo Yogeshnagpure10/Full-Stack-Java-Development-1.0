96a4b4b3c319c5fc27550c123e55f010d4e05f61
Yogeshwar Nagpure <ynagpure1994@gmail.com>
2026-09-06 00:58:23 +0530

    comment pattern81 added

---
+++ practiceProgram/pattern/Pattern81.java
@@ -0,0 +1,37 @@
+
+
+import java.util.Scanner;
+
+public class Pattern81 {
+
+    public static void main(String [] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter n:");
+
+        int n = sc.nextInt();
+
+        for(int i=1 ; i<=n; i++)
+        {
+            for(int j=1 ; j<=n; j++)
+            {
+                if(i==j || i+j==n+1)
+                {
+                    if(j>=(n/2)+2)
+                    {
+                        System.out.print("  ");
+                    }else 
+                    {
+                        System.out.print("* ");
+                    }
+                }else 
+                {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+
+    }
+    
+}
